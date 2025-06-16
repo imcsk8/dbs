b0c16734072e8b9a47ac7d2900890a7a80bdd395
Iván Chavero <ichavero@chavero.com.mx>
2025-06-15 18:28:00 -0600

    Get grup info

--- scripts/sync_distgit.sh
+++ scripts/sync_distgit.sh
@@ -8,9 +8,9 @@
 GROUP_ID=8794173
 
 GROUP_URI="https://gitlab.com/api/v4/groups/${GROUP_ID}/projects"
-CENTOS_DISTGIT_INFO="../repo_info/groups/${GROUP_ID}"
+CENTOS_DISTGIT_PATH="/srv/dbs/repo_info/groups/${GROUP_ID}"
 # Group metadata
-GROUP_INFO="${CENTOS_DISTGIT_INFO}/repo_info.txt"
+GROUP_INFO="${CENTOS_DISTGIT_PATH}/repo_info.txt"
 
 # True if there are new metadata in the repo, false any other way
 REFRESH="false"
@@ -20,12 +20,12 @@ TOTAL_REPOS=0
 # x-total shows the total of repos in the group
 function get_group_info {
 
-    if [[ ! -d $CENTOS_DISTGIT_INFO ]]; then
-        mkdir -p $CENTOS_DISTGIT_INFO
+    if [[ ! -d $CENTOS_DISTGIT_PATH ]]; then
+        mkdir -p $CENTOS_DISTGIT_PATH
     fi
 
     echo "Getting rpm group metadata"
-    tmp_repo_info="${CENTOS_DISTGIT_INFO}/tmp_repo_info.txt"
+    tmp_repo_info="${CENTOS_DISTGIT_PATH}/tmp_repo_info.txt"
 
     # Initialize repo_info.txt
     if [[ ! -f ${GROUP_INFO} ]]; then
@@ -59,7 +59,7 @@ function get_all_repo_info {
     # Guardamos la información de cada repo en un archivo
     while [ 1 ]; do
         echo "Getting page: ${PAGE}"
-        current_page="${CENTOS_DISTGIT_INFO}/centos_distgit_page_${PAGE}.json"
+        current_page="${CENTOS_DISTGIT_PATH}/centos_distgit_page_${PAGE}.json"
         curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" \
             "${GROUP_URI}?per_page=100&page=${PAGE}&with_shared=false" | \
             jq '.[]' > $current_page
@@ -68,12 +68,12 @@ function get_all_repo_info {
             break
         fi
     done
-    jq -s '.' ${CENTOS_DISTGIT_INFO}/centos_distgit_page_* > CENTOS_DISTGIT.json
+    jq -s '.' ${CENTOS_DISTGIT_PATH}/centos_distgit_page_* > ${CENTOS_DISTGIT_PATH}/CENTOS_DISTGIT_REPOS.json
 }
 
 
 function clone_repos {
-    jq -r '.[] | .ssh_url_to_repo' "${CENTOS_DISTGIT_INFO}/CENTOS_DISTGIT.json"
+    jq -r '.[] | .ssh_url_to_repo' "${CENTOS_DISTGIT_PATH}/CENTOS_DISTGIT.json"
 }
 
 echo "Getting group metadata"
@@ -81,7 +81,7 @@ get_group_info
 
 if [[ ${REFRESH} == "true" ]]; then
     echo "Getting all the repos"
-    #get_all_repo_info
+    get_all_repo_info
     echo "Refreshing repos"
 fi
 
