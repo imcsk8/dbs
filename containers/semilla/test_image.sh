7a30c54bb38e58fed70351852d6a99ef4087da07
Iván Chavero <ichavero@chavero.com.mx>
2026-06-30 09:29:56 -0600

    Use kata containers for tests

--- containers/semilla/test_image.sh
+++ containers/semilla/test_image.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-podman run --security-opt seccomp=unconfined \
+podman run --security-opt seccomp=unconfined      \
+    --runtime=krun                                \
     -ti -v /home/imcsk8/tmp/centos_repos:/repos:Z \
-    -v /home/imcsk8/tmp/dbs/root:/root:Z \
-    ghcr.io/imcsk8/semilla:latest \
+    -v /home/imcsk8/tmp/dbs/root:/root:Z          \
+    ghcr.io/imcsk8/semilla:latest                 \
     /bin/bash
