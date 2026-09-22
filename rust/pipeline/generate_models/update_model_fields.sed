3c78ed1ad2de6cb9f0af3d6a5099f07551376647
Iván Chavero <ichavero@chavero.com.mx>
2026-09-22 08:03:38 -0600

    Code refactor

--- rust/pipeline/generate_models/update_model_fields.sed
+++
@@ -1,3 +0,0 @@
-# Add field modifiers
-/pub raw_data: Option<serde_json::Value>/ i \
-    \#[diesel(serialize_as = JsonValue)]
--- rust/pipeline/src/cli/os_actions.rs
+++
@@ -1,8 +0,0 @@
-use super::*; // Granular imports
-use crate::cli::os::AddOsArgs;
-use crate::schema::operating_system::dsl::*;
-use diesel::PgConnection;
-
-pub fn add(conn: &mut PgConnection, args: &AddOsArgs) {
-    println!("In add action!");
-}
